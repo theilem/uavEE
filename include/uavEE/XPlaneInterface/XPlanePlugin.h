51233e09b28d4e4591e00878d595a982bbdd849e
Richard Nai <richardnai6@gmail.com>
2021-02-10 20:21:16 +0100

    style changes, removing global variables

--- include/uavEE/XPlaneInterface/XPlanePlugin.h
+++ include/uavEE/XPlaneInterface/XPlanePlugin.h
@@ -9,33 +9,29 @@
 void
 handler(void* mRef, void* iRef);
 
-void
+int
 registerCommand(XPLMMenuID menuID, const char* name, const char* description,
-				int (* func)(XPLMCommandRef, XPLMCommandPhase, void*), int inBefore = 0, void* inRefcon = NULL);
+				XPLMCommandCallback_f func, int inBefore = 0, void* inRefcon = NULL);
 
 int
-resetConfig(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
+resetConfig(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void*);
 
 int
-startNode(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
+startNode(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void*);
 
 int
 setAutopilotState(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
 
-
-XPLMMenuID
-addDirectoryInfo(XPLMMenuID parentMenu, int menuIdx);
+void
+addDirectoryInfo(XPLMMenuID &configMenu);
 
 void
-configSelector(void* mRef, void* iRef);
+configSelector(void*, void* iRef);
 
 int
 refreshConfigInfo(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
 
 int
-generateConfig(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon);
-
-void
-populateConfig();
+generateConfig(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void*);
 
 #endif //UAVEE_XPLANEPLUGIN_H
